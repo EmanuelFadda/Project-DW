0dcd0c7dd8071ccdb76fd97dcfbebb091bd68155
giodifi <gioeledifi@gmail.com>
2022-02-20 22:12:28 +0100

    Prova funzionamento Git 1

--- src/projectdw/HTMLFiles/script.js
+++ src/projectdw/HTMLFiles/script.js
@@ -2,5 +2,5 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
  */
-
+document.getElementById("");
 
