13f67f7577026615098a42fdab74def11a73553a
EmanuelFadda <faddaemanuel@istitutocorni.it>
2022-02-21 09:58:11 +0100

    add class 'inputSite.java'

---
+++ src/projectdw/InputSite.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectdw;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+/**
+ *
+ * @author USER
+ */
+public class InputSite {
+    private String siteInput;
+
+    public InputSite(String siteInput) {
+        this.siteInput = siteInput;
+    }
+
+    public String getSiteInput() {
+        return siteInput;
+    }
+
+    public void setSiteInput(String siteInput) {
+        this.siteInput = siteInput;
+    }
+    
+    public String restCall() throws MalformedURLException, IOException{
+        URL url = new URL(this.getSiteInput()); //prende l'url
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection(); 
+        conn.setRequestMethod("GET"); //setta il tipo della chiamata, in questo caso "get"
+        conn.setRequestProperty("Accept", "application/json"); //setta le proprietà 
+        if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "+ conn.getResponseCode()); 
+        }
+        BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream()))); //inserisce il risultato della risposta nell'oggetto buffered reader
+         //stringa del risultato
+        String output=br.readLine();
+        if(output != null) {
+            conn.disconnect(); 
+        }
+        return output;
+    }
+ }
--- src/projectdw/ProjectDW.java
+++ src/projectdw/ProjectDW.java
@@ -18,7 +18,8 @@ public class ProjectDW {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        
+       InputSite inputSite=new InputSite("https://dwweb.gnet.it/dw2022/");
+       System.out.println(inputSite.restCall());
     }
     
 }
