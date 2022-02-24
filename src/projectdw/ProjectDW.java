436db39e509461acd4b44f7e99547b14983f2eca
EmanuelFadda <faddaemanuel@istitutocorni.it>
2022-02-24 08:52:31 +0100

    Now api values are linked to script.js

--- src/projectdw/ProjectDW.java
+++ src/projectdw/ProjectDW.java
@@ -7,8 +7,10 @@ package projectdw;
 
 import com.google.gson.Gson;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  *
@@ -23,28 +25,43 @@ public class ProjectDW {
        Gson g=new Gson();
        InputSite inputSite=new InputSite("https://dwweb.gnet.it/dw2022/");
        String s1=inputSite.restCall();
-       // System.out.println(s1);
+
        PolygonCaller pCaller= new PolygonCaller("Zu3H1W9Hu42COZSkkkb5AxiDBPRfGKg8",g.fromJson(s1, Params.class));
-      // System.out.println(pCaller.restCallDataStocks());
-       //System.out.println(pCaller.restCallInfoStocks());
-       //System.out.println(pCaller.restCallDataStocks());
          String str1=pCaller.restCallInfoStocks();
         String str2=pCaller.restCallDataStocks();
-       //  System.out.println(pCaller.restCallInfoStocks());
+       
         InfoStock infoStock=g.fromJson(str1, InfoStock.class);
          Elaborations elaborations=g.fromJson(str2, Elaborations.class );
-       // System.out.println(infoStock);
+
       OutputDataHTML output=new OutputDataHTML(elaborations,infoStock);
        System.out.println(g.toJson(output));
-       /* FileWriter w;
-        w=new FileWriter("jsonData.json");
-        BufferedWriter b;
-        b=new BufferedWriter (w);*/
-
-     //b.write();
-       // System.out.println(elaborations);
-       // HTMLLoader a=new HTMLLoader("src\\projectdw\\HTMLFiles\\index.html");
-       // a.loadFileHTML();
+       
+       
+       
+        //Instantiating the File class
+      String filePath = "src\\\\projectdw\\\\HTMLFiles\\\\script.js";
+      //Instantiating the Scanner class to read the file
+      Scanner sc = new Scanner(new File(filePath));
+      //instantiating the StringBuffer class
+      StringBuffer buffer = new StringBuffer();
+      //Reading lines of the file and appending them to StringBuffer
+      while (sc.hasNextLine()) {
+         buffer.append(sc.nextLine()+System.lineSeparator());
+      }
+      String fileContents = buffer.toString();
+      System.out.println("Contents of the file: "+fileContents);
+      //closing the Scanner object
+      sc.close();
+      String oldLine = "let JSONData=''";
+      String newLine = "let JSONData='"+g.toJson(output)+"'";
+      //Replacing the old line with new line
+      fileContents = fileContents.replaceAll(oldLine, newLine);
+      FileWriter writer = new FileWriter(filePath);
+      writer.append(fileContents);
+      writer.flush();
+      HTMLLoader html =new HTMLLoader("src\\\\projectdw\\\\HTMLFiles\\\\index.html");
+      html.loadFileHTML();
+      
     }
     
 }
